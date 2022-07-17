2724766c7e47c532a307e6d87a1115ad84ca94a3
rafiatun <lubabarafiatun@gmail.com>
2022-07-18 00:00:30 +0600

    second

--- Main/views.py
+++ Main/views.py
@@ -7,7 +7,7 @@ from .forms import *
 # Create your views here.
 
 def Portfolio(request):
-    About="I am Rafiatun Ferdous Khan Lubaba, an undergraduate student at Chittagong University of Engineering and Technology's Department of Mechatronics and Industrial Engineering. I'm a technologist that appreciates learning new things outside of my academic life. In order to develop myself, I've been studying new subjects on a regular basis. For the past two years, I've been studying Python, Machine Learning, Deep Learning, Django, Front End Development, and Uipath."
+    About="I am Rafiatun Ferdous, an undergraduate student at Chittagong University of Engineering and Technology's Department of Mechatronics and Industrial Engineering. I'm a technologist that appreciates learning new things outside of my academic life. In order to develop myself, I've been studying new subjects on a regular basis. For the past two years, I've been studying Python, Machine Learning, Deep Learning, Django, Front End Development, and Uipath."
     About_2="I've been working hard to improve myself on a regular basis. During my early years of university, I worked on some automation and web development projects and competed in a few contests. I'm actively working with organisations to enhance both my technical and soft abilities.As I previously indicated, I am involved in a variety of disciplines, and I also own an e-magazine platform called শোলোক। "
     skills=Skills.objects.all()
     education=Education.objects.all()
